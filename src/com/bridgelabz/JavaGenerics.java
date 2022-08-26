7341c65bccd9dae7cca3a711715a254c768c4acc
nayana005 <nayanasuresh1005@gmail.com>
2022-08-27 02:00:59 +0530

    Added: Find Max String

--- src/com/bridgelabz/JavaGenerics.java
+++ src/com/bridgelabz/JavaGenerics.java
@@ -4,8 +4,12 @@ public class JavaGenerics {
 
 	public static void main(String[] args) {
 		System.out.println("Maximum Problem");
-		Integer a = 10, b = 20, c = 15;
-		findMax(a,b,c);
+		Integer i1 = 10, i2 = 20, i3 = 15;
+		Float f1 = 100.30f, f2 = 230.34f, f3 = 173.945f ;
+		String s1 = "APPLE", s2 = "PEACH", s3 = "BANANA";
+		findMax(i1,i2,i3);
+		findMax(f1,f2,f3);
+		findMax(s1,s2,s3);
 	}
 
 	public static Integer findMax(Integer a, Integer b , Integer c){
@@ -16,7 +20,31 @@ public class JavaGenerics {
 		if(c.compareTo(maximum) > 0){
 			maximum = c;
 		}
-		System.out.println("The Maximum Integer from " + a + " "+ " " + b + " " + c + " is: " + maximum);
+		System.out.println("The Maximum Integer from " + a + " " + " " + b + " " + c + " is: " + maximum);
+		return maximum;
+	}
+
+	public static Float findMax(Float a, Float b, Float c){
+		Float maximum = a;
+		if(b.compareTo(maximum) > 0){
+			maximum = b;
+		}
+		if(c.compareTo(maximum) > 0){
+			maximum = c;
+		}
+		System.out.println("The Maximum Float Number from " + a + " " + " " + b + " " + c + " is: " + maximum);
+		return maximum;
+	}
+
+	public static String findMax(String a, String b, String c){
+		String maximum = a;
+		if(b.compareTo(maximum) > 0){
+			maximum = b;
+		}
+		if(c.compareTo(maximum) > 0){
+			maximum = c;
+		}
+		System.out.println("The Maximum String from " + a + " " + " " + b + " " + c + " is: " + maximum);
 		return maximum;
 	}
 }
