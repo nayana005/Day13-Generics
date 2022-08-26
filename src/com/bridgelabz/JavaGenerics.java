fbfdef9f26c7ef8f6f6e439e663132e3f54bbb66
nayana005 <nayanasuresh1005@gmail.com>
2022-08-27 01:55:55 +0530

    Added: Find Max Float

--- src/com/bridgelabz/JavaGenerics.java
+++ src/com/bridgelabz/JavaGenerics.java
@@ -4,8 +4,10 @@ public class JavaGenerics {
 
 	public static void main(String[] args) {
 		System.out.println("Maximum Problem");
-		Integer a = 10, b = 20, c = 15;
-		findMax(a,b,c);
+		Integer i1 = 10, i2 = 20, i3 = 15;
+		Float f1 = 100.30f, f2 = 230.34f, f3 = 173.945f ;
+		findMax(i1,i2,i3);
+		findMax(f1,f2,f3);
 	}
 
 	public static Integer findMax(Integer a, Integer b , Integer c){
@@ -17,6 +19,18 @@ public class JavaGenerics {
 			maximum = c;
 		}
 		System.out.println("The Maximum Integer from " + a + " "+ " " + b + " " + c + " is: " + maximum);
+		return maximum;
+	}
+
+	public static Float findMax(Float a, Float b, Float c){
+		Float maximum = a;
+		if(b.compareTo(maximum) > 0){
+			maximum = b;
+		}
+		if(c.compareTo(maximum) > 0){
+			maximum = c;
+		}
+		System.out.println("The Maximum Float Number from " + a + " "+ " " + b + " " + c + " is: " + maximum);
 		return maximum;
 	}
 }
